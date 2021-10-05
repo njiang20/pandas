4a0d973dd70d1363c52b3cfa788c32c0709ce90a
Nina Jiang <njiang20@stuy.edu>
2021-10-04 21:21:31 -0400

    initsetup

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,5 +1,5 @@
 public class BigSib {
-	public static System greet(String name) {
+	public static void greet(String name) {
 		System.out.println("Why, hello there, " + name + ". How do you do?");
 	}
 }
