454e5e97e6242a5f7db4176ccba43f8c296d52d0
Nina Jiang <njiang20@stuy.edu>
2021-10-07 19:49:51 -0400

    initsetup

---
+++ 14_overload/defcon/Greet.java
@@ -0,0 +1,17 @@
+Greet {
+	main( String[] args){
+		String greeting;
+		BigSib richard = new BigSib0);
+		BigSib grizz = new BigSib();
+		BigSib dotCom = new BigSib() ;
+		BigSib tracy = new BigSib();
+		greeting = richard.greet("freshman");
+		System.out.println(greeting);
+		greeting = tracy.greet("Dr.Spaceman" );
+		System.out.println(greeting);
+		greeting = grizz.greet("Kong Fooey");
+		System.out.println(greeting);
+		greeting = dotCom.greet("mom");
+		System. out.println(greeting);
+	};
+}; 
