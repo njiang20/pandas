4c2694375e5878eccf34ea1be5f2be371eb9530d
Nina Jiang <njiang20@stuy.edu>
2021-10-07 21:03:08 -0400

    initsetup

--- 14_overload/defcon/Greet.java
+++ 14_overload/defcon/Greet.java
@@ -1,10 +1,12 @@
-Greet {
-	main( String[] args){
+public class Greet {
+	public static void main(String[] args){
 		String greeting;
-		BigSib richard = new BigSib0);
+
+		BigSib richard = new BigSib();
 		BigSib grizz = new BigSib();
-		BigSib dotCom = new BigSib() ;
+		BigSib dotCom = new BigSib();
 		BigSib tracy = new BigSib();
+
 		greeting = richard.greet("freshman");
 		System.out.println(greeting);
 		greeting = tracy.greet("Dr.Spaceman" );
@@ -13,5 +15,5 @@ Greet {
 		System.out.println(greeting);
 		greeting = dotCom.greet("mom");
 		System. out.println(greeting);
-	};
-}; 
+	}
+}
--- 14_overload/overcon/Greet.java
+++ 14_overload/overcon/Greet.java
@@ -1,17 +1,19 @@
-Greet {
-	main( String[] args ) {
+public class Greet {
+	public static main(String[] args) {
 		String greeting;
-		BigSib richard = new BigSib("Word up" ) ;
-		BigSib grizz = new BigSib("Hey ya" ) ;
-		BigSib dotCom = new BigSib("Sup") ;
-		BigSib tracy = new BigSib("Salutations);
-		greeting = richard.greet(" freshman' );
+
+		BigSib richard = new BigSib("Word up");
+		BigSib grizz = new BigSib("Hey ya");
+		BigSib dotCom = new BigSib("Sup");
+		BigSib tracy = new BigSib("Salutations");
+
+		greeting = richard.greet("freshman");
 		System.out.println(greeting);
-		greeting = tracy.greet("Dr.Spaceman" );
+		greeting = tracy.greet("Dr.Spaceman");
 		System.out.println(greeting);
-		greeting = grizz.greet(" Kong Fooey");
+		greeting = grizz.greet("Kong Fooey");
 		System.out.println(greeting);
-		greeting = dotCom.greet(" mom" ) ;
+		greeting = dotCom.greet("mom");
 		System.out.println(greeting);
-	};
-};
+	}
+}
