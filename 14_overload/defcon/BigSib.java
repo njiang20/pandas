78043309e5ccbf5e740dc4002410b4805715ada9
Nina Jiang <njiang20@stuy.edu>
2021-10-08 13:26:55 -0400

    initsetup

---
+++ 14_overload/defcon/BigSib.java
@@ -0,0 +1,13 @@
+public class BigSib{
+	// instance variable
+	String helloMsg;
+
+	// constructor
+	public BigSib(){
+		helloMsg = "Word up";
+	}
+
+	public String greet(String name){
+    		return(helloMsg + " " + name);
+	}
+}
