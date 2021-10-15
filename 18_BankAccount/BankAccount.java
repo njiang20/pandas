30739cf3358e073f066b01c315a9de74038ec9e7
Nina Jiang <njiang20@stuy.edu>
2021-10-14 20:29:42 -0400

    create BankAcc

--- 18_BankAccount/BankAccount.java
+++ 18_BankAccount/BankAccount.java
@@ -1,3 +1,14 @@
+/*
+Triumphant Triangles--Nina Jiang, Jason Zhou, John Gupta-she
+APCS
+HW18--CPA-One
+2021-10-14
+
+DISCO:
+
+QCC:
+*/
+
 public class BankAccount {
 	//vars
 	private double balance;
@@ -5,10 +16,10 @@ public class BankAccount {
 	private String accName;
 	private String accPss;
 	private String accId;
-	
+
 	//constructor
 	public BankAccount() {
-		
+
 	}
 	//overloaded constructor
 	public BankAccount(String name, String pss, String id, String pinnum, double setBalance) {
@@ -16,11 +27,11 @@ public class BankAccount {
 		accPin = pinnum;
 		accName = name;
 		accPss = pss;
-		
+
 		//Randomize?
 		accId = id;
 	}
-	
+
 	//methods
 	public String getInfo(String pss, String pin) {
 		//.equals/compareTo
@@ -32,42 +43,51 @@ public class BankAccount {
 	}
 	/*
 	public String getInfo(String name, String pss, String pin) {
-		return 
+		return
 	}
 	*/
-	
+
 	public void changePin(String oldPin, String newPin) {
 		//add superuser
 		if (oldPin == accPin) {
 			accPin = newPin;
+		} else {
+			System.out.println("Wrong Pin");
 		}
 	}
-	
+
 	public void changeName(String newName, String pss) {
 		if (pss == accPss) {
 			accName = newName;
+		} else {
+			System.out.println("Wrong Password");
 		}
-			
 	}
-	
+
 	public void changePsswrd(String oldPss, String newPss, String newPssRepeat) {
 		if (oldPss == accPss && newPss == newPssRepeat) {
 			accPss = newPss;
+		} else {
+			System.out.println("Wrong Password");
 		}
 	}
-	
+
 	public void deposit(String pin, double amount) {
 		if (pin == accPin && amount > 0) {
 			balance += amount;
+		} else {
+			System.out.println("Wrong Pin");
 		}
 	}
-	
+
 	public void withdraw(String pin, double amount) {
 		if (pin == accPin && amount > 0) {
 			balance -= amount;
+		} else {
+			System.out.println("Wrong Pin");
 		}
 	}
+
 	public static void main(String[] args) {
 	}
-	
 }
