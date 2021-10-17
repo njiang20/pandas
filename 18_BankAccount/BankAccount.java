1f986b480392f8f7c8bfd681a7f869892135c92b
Nina Jiang <njiang20@stuy.edu>
2021-10-17 18:09:02 -0400

    create BankAcc

--- 18_BankAccount/BankAccount.java
+++ 18_BankAccount/BankAccount.java
@@ -8,6 +8,9 @@ DISCO:
 -we don't need to ask for account name everytime since it's already set to elmo
 QCC:
 -What is a superclass and can there be one here?
+
+Q2: Constructors are used whenever you create an instance of an object. Hence, it Java did not create a constructor for you, BigSib.java would not work.
+Q3: An easy test that we can do is create an instance of a class, say BankAccount and run instanceOfBankAccount.toString(); This will return ClassName@hashcode. This isn't really useful besides for debugging and other things. The superclass to all classes is object class. Class Object has a method toString() which converts an object into a string. Since every class is a subclass of Object, we can do @Override and override the method in our subclass.
 */
 
 public class BankAccount {
