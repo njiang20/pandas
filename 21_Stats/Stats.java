cba5f6fd499cb2a726808f53788fa26b765b2ad8
Nina Jiang <njiang20@stuy.edu>
2021-10-20 19:01:21 -0400

    hw21

--- 21_Stats/Stats.java
+++ 21_Stats/Stats.java
@@ -1,5 +1,4 @@
 public class Stats {
-
 	public static int mean(int a, int b) {
 		int mean = ((a + b)/2);
 		return mean;
@@ -31,7 +30,7 @@ public class Stats {
 	}
 
 	public static int geoMean(int a, int b) {
-		int geoMean = Math.pow((a * b), 1/2);
+		int geoMean = Math.sqrt((a * b), 1/2);
 		return geoMean;
 	}
 
@@ -41,7 +40,7 @@ public class Stats {
 	}
 
 	public static int max(int a, int b, int c) {
-		if (a >= b >= c) {
+		if (a >= b && b >= c) {
 			return a;
 		} else if (b >= a && b >= c) {
 			return b;
@@ -51,7 +50,7 @@ public class Stats {
 	}
 
 	public static double max(double a, double b, double c) {
-		if (a >= b >= c) {
+		if (a >= b && b >= c) {
 			return a;
 		} else if (b >= a && b >= c) {
 			return b;
@@ -72,6 +71,23 @@ public class Stats {
 
 	//main method for testing functionality
 	public static void main( String[] args ) {
+		System.out.println(Stats.mean(1,2));
+		System.out.println(Stats.mean(1.0,2.0));
 
+		System.out.println(Stats.max(1,10));
+		System.out.println(Stats.max(1,4.5));
+
+		System.out.println(Stats.geoMean(9,16));
+		System.out.println(Stats.geoMean(9.0,16.0));
+
+		System.out.println(Stats.max(1,2,3));
+		System.out.println(Stats.max(1.0,2.0,3.0));
+		System.out.println(Stats.max(-1,2,3));
+		System.out.println(Stats.max(0,1,2));
+		System.out.println(Stats.max(1,1,1));
+		System.out.println(Stats.max(2.2,2.2,2.2));
+
+		System.out.println(Stats.geoMean(4,9,16));
+		System.out.println(Stats.geoMean(4.0,9.0,16.0));
 	}
-}//end class
+}
