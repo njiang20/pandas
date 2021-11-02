c48bd540e740e2494fbc768159a6a9713de01526
Nina Jiang <njiang20@stuy.edu>
2021-11-01 20:17:29 -0400

    hw28

--- cb/string-1/countHi.java
+++
@@ -1,9 +0,0 @@
-public int countHi(String str) {
-  int numHi = 0;
-  for (int x = 0; x < str.length() - 1; x++) {
-    if (str.charAt(x)== 'h' && str.charAt(x + 1)=='i') {
-      return 
-    }
-  }
-}
-
