a3c06f29268f7785589dec7815e285790cdba2e7
Nina Jiang <njiang20@stuy.edu>
2021-11-16 14:03:53 -0500

    hw37

--- cb/array-2/countEvens.java
+++ cb/array-2/countEvens.java
@@ -1,6 +1,6 @@
 /* Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1. */
 public class countEvens {
-  public int countEvens(int[] nums) {
+  public static int countEvens(int[] nums) {
     int counter = 0;
     for(int i = 0; i < nums.length; i++){
       if(nums[i] % 2 == 0){
@@ -9,5 +9,9 @@ public class countEvens {
     }
     return counter;
   }
-
+  public static void main(String[] args) {
+    System.out.println(countEvens(new int[] {2, 1, 2, 3, 4}));
+    System.out.println(countEvens(new int[] {2, 2, 0}));
+    System.out.println(countEvens(new int[] {1, 3, 5}));
+  }
 }
