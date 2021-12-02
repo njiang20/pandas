1fca38e47910e1a69fc8c8367441786734fda855
Nina Jiang <90851568+njiang20@users.noreply.github.com>
2021-12-01 20:54:38 -0500

    hw41

--- 41_rat/Rational.java
+++ 41_rat/Rational.java
@@ -4,6 +4,12 @@ APCS pd08
 HW41 -- Be Rational
 2021-12-01
 Time Spent: 0.5 hrs
+
+
+DISCO: you can only call the default constructor in an overloaded constructor in the first statement;
+       avoid division in writing multiply/divide methods (for divide, multiply by the reciprocal);
+       you can access another instance's private variable within a method of the same class type
+QCC:   what is problematic about calling a default constructor later in the overloaded constructor?
 */
 
 public class Rational {
@@ -16,21 +22,21 @@ public class Rational {
 	}
 
 	public Rational(int num, int den) {
-		numerator = num;
 		if (den != 0) {
 			denominator = den;
 		} else {
-      numerator = 0;
-      denominator = 1;
+			numerator = 0;
+			denominator = 1;
 		}
+		numerator = num;
 	}
 
 	public String toString() {
 		return numerator + "/" + denominator;
 	}
 
-	public float floatValue() {
-		return (float)numerator/denominator;
+	public double floatValue() {
+		return (double)numerator/denominator;
 	}
 
 	public void multiply(Rational n) {
@@ -45,15 +51,15 @@ public class Rational {
 
 	public static void main(String[] args) {
 		Rational a = new Rational(1, 7);
-    Rational b = new Rational(2, 6);
-
+		Rational b = new Rational(2, 6);
 		System.out.println(a.toString());
-    System.out.println(a.floatValue());
 
-    a.multiply(b);
-    System.out.println(a.toString());
+		System.out.println(a.floatValue());
 
-    a.divide(b);
-    System.out.println(a.toString());
+		a.multiply(b);
+		System.out.println(a.toString());
+
+		a.divide(b);
+		System.out.println(a.toString());
 	}
 }
