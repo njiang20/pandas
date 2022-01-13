ca0ab9498ef6536c6eac3fb3e83e49cc3d4154d9
Nina Jiang <90851568+njiang20@users.noreply.github.com>
2022-01-12 19:34:34 -0500

    magpie

--- cobo/magpie/activity4/Magpie4.java
+++ cobo/magpie/activity4/Magpie4.java
@@ -31,6 +31,7 @@ public class Magpie4
 	public String getResponse(String statement)
 	{
 		String response = "";
+		int iPsn = findKeyword(statement, "I", 0);
 		if (statement.length() == 0)
 		{
 			response = "Say something, please.";
@@ -56,6 +57,10 @@ public class Magpie4
 
 		else if (findKeyword(statement, "I want", 0) >= 0) {
 			response = transformIWantStatement(statement);
+		}
+
+		else if (iPsn >= 0 && findKeyword(statement, "you", iPsn) >= 0) {
+			response = transformIYouStatement(statement);
 		}
 
 		else
@@ -130,16 +135,32 @@ public class Magpie4
 	private String transformIWantStatement(String statement) {
 		statement = statement.trim();
 		String end = statement.substring(statement.length() - 1);
+
 		if (end.equals(".") || end.equals("!")) {
 			statement = statement.substring(0, statement.length() - 1);
 		}
+
 		int psn = findKeyword (statement, "I want", 0);
 		String restOfStatement = statement.substring(psn + 6).trim();
+
 		return "Would you really be happy if you had " + restOfStatement + "?";
 	}
 
 
+	private String transformIYouStatement(String statement) {
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
 
+		if (lastChar.equals(".")){
+			statement = statement.substring(0, statement.length() - 1);
+		}
+
+		int psnOfI = findKeyword (statement, "I", 0);
+		int psnOfYou = findKeyword (statement, "you", psnOfI + 1);
+		String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
+
+		return "Why do you " + restOfStatement + " me?";
+	}
 
 
 	/**
