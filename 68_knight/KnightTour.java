289e628a5b5d0482c2cfac7da68cc92320e90862
Nina Jiang <njiang20@stuy.edu>
2022-03-01 13:19:04 -0500

    hw68

---
+++ 68_knight/KnightTour.java
@@ -0,0 +1,79 @@
+// Clyde Sinclair
+// APCS pd0
+// HW68 -- recursively probing for a closed cycle
+// 2022-02-28m
+// time spent:  hrs
+
+/***
+ * SKELETON
+ * class KnightTour (and supporting class TourFinder)
+ * Animates generation of a Knight's Tour on a square chess board.
+ *
+ * USAGE: (default N value: 8)
+ * $ javac KnightTour.java
+ * $ java KnightTour
+ * $ java KnightTour [N]
+ *
+ * ALGO
+ *
+ * DISCO
+ *
+ * QCC
+ *
+ * Mean execution times for boards of size n*n:
+ * n=5   __s    across __ executions
+ * n=6   __s    across __ executions
+ * n=7   __s    across __ executions
+ * n=8   __s    across __ executions
+ *
+ * POSIX PROTIP: to measure execution time from BASH, use time program:
+ * $ time java KnightTour 5
+ *
+ ***/
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class KnightTour
+{
+  public static void main( String[] args )
+  {
+    int n = 8;
+
+    try {
+      n = Integer.parseInt( args[0] );
+    } catch( Exception e ) {
+      System.out.println( "Invalid input. Using board size "
+                          + n + "..." );
+    }
+
+    TourFinder tf = new TourFinder( n );
+
+    //clear screen using ANSI control code
+    System.out.println( "[2J" );
+
+    //display board
+    System.out.println( tf );
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //for fixed starting location, use line below:
+    tf.findTour( 2, 2, 1 );
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //for random starting location, use lines below:
+    //int startX = //YOUR MATH CONSTRUCT FOR GENERATING A RANDOM LEGAL X VALUE
+    //int startY = //YOUR MATH CONSTRUCT FOR GENERATING A RANDOM LEGAL X VALUE
+    //tf.findTour( startX, startY, 1 );   // 1 or 0 ?
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // PUSHING FARTHER...
+    // Systematically attempt to solve from every position on the board?
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  }//end main()
+
+}//end class KnightTour
