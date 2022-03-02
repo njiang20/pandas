95e3ba5518e820dff93f11ad41ca5b491bcfa49b
Nina Jiang <90851568+njiang20@users.noreply.github.com>
2022-03-01 21:33:35 -0500

    hw68

--- 68_knight/KnightTour.java
+++ 68_knight/KnightTour.java
@@ -77,3 +77,141 @@ public class KnightTour
   }//end main()
 
 }//end class KnightTour
+
+class TourFinder
+{
+  //instance vars
+  private int[][] _board;
+  private int _sideLength; //board has dimensions n x n
+  private boolean _solved = true;
+
+  //constructor -- build board of size n x n
+  public TourFinder( int n )
+  {
+    _sideLength = n;
+
+    //init 2D array to represent square board with moat
+    _board = new int[_sideLength + 4][_sideLength + 4];
+
+    //SETUP BOARD --  0 for unvisited cell
+    //               -1 for cell in moat
+    //---------------------------------------------------------
+    //???
+    for(int i = 0; i < _sideLength + 4; i++) {
+      for(int j = 0; j < _sideLength + 4; j++) {
+        _board[i][j] = -1;
+      }
+    }
+
+    for(int i = 2; i < _sideLength + 2; i++) {
+      for(int j = 2; j < _sideLength + 2; j++) {
+        _board[i][j] = 0;
+      }
+    }
+
+     //-----------
+    //---------------------------------------------------------
+
+  }//end constructor
+
+
+  /**
+   * "stringify" the board
+   **/
+  public String toString()
+  {
+    //send ANSI code "ESC[0;0H" to place cursor in upper left
+    String retStr = "[0;0H";
+    //emacs shortcut: C-q, then press ESC
+    //emacs shortcut: M-x quoted-insert, then press ESC
+
+    int i, j;
+    for( i=0; i < _sideLength+4; i++ ) {
+      for( j=0; j < _sideLength+4; j++ )
+        retStr = retStr + String.format( "%3d", _board[j][i] );
+      //"%3d" allots 3 spaces for each number
+      retStr = retStr + "\n";
+    }
+    return retStr;
+  }
+
+
+  /**
+   * helper method to keep try/catch clutter out of main flow
+   * @param n      delay in ms
+   **/
+  private void delay( int n )
+  {
+    try {
+      Thread.sleep(n);
+    } catch( InterruptedException e ) {
+      System.exit(0);
+    }
+  }
+
+
+  /**
+   * void findTour(int x,int y,int moves) -- use depth-first w/ backtracking algo
+   * to find valid knight's tour
+   * @param x      starting x-coord
+   * @param y      starting y-coord
+   * @param moves  number of moves made so far
+   **/
+  public void findTour( int x, int y, int moves )
+  {
+    //delay(50); //slow it down enough to be followable
+
+    //if a tour has been completed, stop animation
+    if ( _solved ) System.exit(0);
+
+    //primary base case: tour completed
+    if ( moves == (_board[0].length) * (_board[0].length) + 1  ) {
+      //???
+      System.out.println( this ); //refresh screen
+      return;
+    }
+    //other base case: stepped off board or onto visited cell
+    if ( x > _sideLength ) {
+      return;
+    }
+    //otherwise, mark current location
+    //and recursively generate tour possibilities from current pos
+    else {
+
+      //mark current cell with current move number
+      _board[x][y] = moves;
+
+      System.out.println( this ); //refresh screen
+
+      //delay(1000); //uncomment to slow down enough to view
+
+      /******************************************
+       * Recursively try to "solve" (find a tour) from
+       * each of knight's available moves.
+       *     . e . d .
+       *     f . . . c
+       *     . . @ . .
+       *     g . . . b
+       *     . h . a .
+      ******************************************/
+      //???
+
+      findTour(x+1, y+2, moves+1);
+      findTour(x+2, y+1, moves+1);
+      findTour(x+2, y-1, moves+1);
+      findTour(x+1, y-2, moves+1);
+      findTour(x-1, y-2, moves+1);
+      findTour(x-2, y-1, moves+1);
+      findTour(x-2, y+1, moves+1);
+      findTour(x-1, y+2, moves+1);
+
+      //If made it this far, path did not lead to tour, so back up...
+      // (Overwrite number at this cell with a 0.)
+        //???
+        _board[x][y] = 0;
+
+      System.out.println( this ); //refresh screen
+    }
+  }//end findTour()
+
+}//end class TourFinder
