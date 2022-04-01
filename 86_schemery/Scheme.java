6bb4655c2988a306606f0b0010197344ec00cfad
Nina Jiang <90851568+njiang20@users.noreply.github.com>
2022-03-31 22:23:31 -0400

    hw86

--- 86_schemery/Scheme.java
+++ 86_schemery/Scheme.java
@@ -1,21 +1,30 @@
+/*
+Team A Random Word (Nina Jiang, Orion Roven, Ivina Wang )
+APCS pd7
+HW86-- What a Racket
+2022-04-01f
+time spent: 1 hrs
+*/
+
 /***
  * class Scheme
  * Simulates a rudimentary Scheme interpreter
  *
  * ALGORITHM for EVALUATING A SCHEME EXPRESSION:
- *   1. Steal underpants.
- *   2. ...
- *   5. Profit!
+ *   1. Push everything until the first closed parenthesis into stack 1
+ *   2. Push the top item of stack 1 into stack 2 until you reach an operation and remove the items from stack 1
+ *   3. Evaluate stack 2 and push it to stack 1.
+ *   4. Repeat this process until there is one number in stack 1 and return it
  *
- * STACK OF CHOICE: ____ by ____
- * b/c ...
+ * STACK OF CHOICE: ALStack by Team Random Word :_)
+ * In the end it didn't matter b/c they all have the same methods/functionality.
  **/
 
 public class Scheme
 {
   /***
    * precond:  Assumes expr is a valid Scheme (prefix) expression,
-   *           with whitespace separating all operators, parens, and 
+   *           with whitespace separating all operators, parens, and
    *           integer operands.
    * postcond: Returns the simplified value of the expression, as a String
    * eg,
@@ -24,7 +33,43 @@ public class Scheme
    **/
   public static String evaluate( String expr )
   {
+    String[] ret = expr.split("//s+");
 
+    Stack<String> turtle = new ALStack();
+    Stack<String> ops = new ALStack();
+    Stack<String> thisThing = new ALStack();
+
+    for (String e : ret) {
+      if (e.equals("-") || e.equals("+") || e.equals("*")) {
+        ops.push(e);
+      } else if (e.equals("(") || isNumber(e)) {
+        turtle.push(e);
+      } else if (e.equals (")")) {
+        if(ops.peekTop().equals("+")) {
+          while(!turtle.peekTop().equals("(")) {
+            thisThing.push(turtle.pop());
+          }
+          turtle.pop();
+          ops.pop();
+          turtle.push(unload(1, thisThing));
+        } else if (ops.peekTop().equals("-")) {
+          while(!turtle.peekTop().equals("(")) {
+            thisThing.push(turtle.pop());
+          }
+          turtle.pop();
+          ops.pop();
+          turtle.push(unload(2, thisThing));
+        } else if(ops.peekTop().equals("*")) {
+          while(!turtle.peekTop().equals("(")) {
+            thisThing.push(turtle.pop());
+          }
+          turtle.pop();
+          ops.pop();
+          turtle.push(unload(3, thisThing));
+        }
+      } //end else
+    } //end for loop
+    return thisThing.peekTop() + ""; //???
   }//end evaluate()
 
 
@@ -36,29 +81,38 @@ public class Scheme
    **/
   public static String unload( int op, Stack<String> numbers )
   {
-
+    int panda = Integer.parseInt(numbers.peekTop());
+    numbers.pop();
+    while(numbers.peekTop() != "(") {
+      if(op == 1) {
+        panda += Integer.parseInt(numbers.pop());
+      } else if (op == 2) {
+        panda -= Integer.parseInt(numbers.pop());
+      } else if (op == 3) {
+        panda *= Integer.parseInt(numbers.pop());
+      }
+    } //end while
+    return panda + "";
   }//end unload()
 
 
-  /*
   //optional check-to-see-if-its-a-number helper fxn:
   public static boolean isNumber( String s ) {
-  try {
-  Integer.parseInt(s);
-  return true;
+    try {
+      Integer.parseInt(s);
+      return true;
+    }
+      catch( NumberFormatException e ) {
+      return false;
+    }
   }
-  catch( NumberFormatException e ) {
-  return false;
-  }
-  }
-  */
+
 
 
   //main method for testing
   public static void main( String[] args )
   {
 
-    /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
       String zoo1 = "( + 4 3 )";
       System.out.println(zoo1);
       System.out.println("zoo1 eval'd: " + evaluate(zoo1) );
@@ -78,6 +132,7 @@ public class Scheme
       System.out.println(zoo4);
       System.out.println("zoo4 eval'd: " + evaluate(zoo4) );
       //...-4
+      /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
       ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
   }//main()
 
