21e62d64e17622d5520372c1e1530f6f9aad6e36
Nina Jiang <90851568+njiang20@users.noreply.github.com>
2022-04-06 16:02:35 -0400

    data

--- cobo/100_data/DataStudentCode/Welcome01.java
+++ cobo/100_data/DataStudentCode/Welcome01.java
@@ -1,10 +1,16 @@
+/* Team Fried Chicken :: Nina Jiang, Lea Kwok, Lindsey Phung
+APCS
+LAB08 -- America's Next Top Data Scientist
+2022-04-06
+time spent: 02.3 hrs */
+
 import core.data.*;
 
 public class Welcome01 {
    public static void main(String[] args) {
-      String id = "KATL";
-      DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/" + id + ".xml"); 
-      ds.setCacheTimeout(15 * 60);  
+      String id = "KNYC";
+      DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/" + id + ".xml");
+      ds.setCacheTimeout(15 * 60);
       ds.load();
       //ds.printUsageString();
       float temp = ds.fetchFloat("temp_f");
--- cobo/100_data/DataStudentCode/Welcome03_List.java
+++ cobo/100_data/DataStudentCode/Welcome03_List.java
@@ -1,4 +1,10 @@
 /*
+Team Fried Chicken :: Nina Jiang, Lea Kwok, Lindsey Phung
+APCS
+LAB08 -- America's Next Top Data Scientist
+2022-04-06
+time spent: 02.3 hrs
+
  * Arrays of objects
  */
 
@@ -9,19 +15,35 @@ import java.util.Scanner;
 public class Welcome03_List {
    public static void main(String[] args) {
       DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/index.xml").load();
-      ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name", 
+      ds.printUsageString();
+      ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name",
              "station/station_id", "station/state",
              "station/latitude", "station/longitude");
       System.out.println("Total stations: " + allstns.size());
-      
+
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter a state abbreviation: ");
       String state = sc.next();
       System.out.println("Stations in " + state);
+
+      ArrayList<WeatherStation> stateStations = new ArrayList<WeatherStation>();
       for (WeatherStation ws : allstns) {
          if (ws.isLocatedInState(state)) {
             System.out.println("  " + ws.getId() + ": " + ws.getName());
+            stateStations.add(ws);
          }
       }
+
+
+      double mostSouth = allstns.get(0).getLat();
+      int loc = 0;
+      for(int i = 0; i < stateStations.size(); i++){
+        if(stateStations.get(i).getLat() < mostSouth){
+          mostSouth = stateStations.get(i).getLat();
+          loc = i;
+        }
+      }
+      System.out.println("The most south station is: " + stateStations.get(loc).getName());
+
    }
 }
