97aea74a99a1fb65abcc2bdb7af64f6b624a46cc
Nina Jiang <njiang20@stuy.edu>
2022-04-11 13:16:06 -0400

    hw89

---
+++ 89_queue/LLQueue.java
@@ -0,0 +1,27 @@
+public class LLQueue<T> implements Queue<T> {
+  public 
+
+  public LLQueue() {
+
+  }
+
+  public T dequeue() {
+
+  }
+
+  public void enqueue( QUASAR x ){
+
+  }
+
+  public boolean isEmpty(){
+
+  }
+
+  public T peekFront(){
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
